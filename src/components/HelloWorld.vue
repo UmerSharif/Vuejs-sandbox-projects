a761b947224ae7a04446429e6f03b235ceb7da44
UmerSK <umer.kdev@gmail.com>
2019-01-02 03:06:41 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>First Name : {{ firstname }}</p>
+    <p>Last Name : {{ lastname }}</p>
+    <hr />
+    <p>{{ fullname }}</p>
+    <button @click="changename">ChangeName</button>
+    <button @click="changenameSetter">Change Name with Setter</button>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      firstname: "umer",
+      lastname: "khan"
+    };
+  },
+  methods: {
+    changename() {
+      this.firstname = "bob";
+      this.lastname = "bambbozled";
+    },
+
+    changenameSetter() {
+      this.fullname = "Jazzy uzzy";
+    }
+  },
+  computed: {
+    fullname: {
+      get() {
+        return `Full Name : ${this.firstname + " " + this.lastname}`;
+      },
+
+      set(newVal) {
+        alert("setting Name :" + newVal);
+        let newName = newVal.split(" ");
+        this.firstname = newName[0];
+        this.lastname = newName[newName.length - 1];
+        alert(this.firstname + " " + this.lastname);
+      }
+    }
+  }
+
+  // computed: {
+  //  fullname() {
+  //       return `Full Name : ${this.firstname + " " + this.lastname}`;
+  //     }
+  // }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/tuts.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="hello">
+    <p>{{ counter }}</p>
+    <p>{{ fullName }}</p>
+    <p>{{ fullName }}</p>
+
+    <button v-on:click="counter++;">Increase Counter</button>
+    <button v-on:click="changeName">Change Name</button>
+    <button v-on:click="changeNameSetter">Change Name (setter)</button>
+  </div>
+</template>
+
+<script>
+export default {
+  //name: "HelloWorld",
+  data() {
+    return {
+      counter: 0,
+      firstName: "Bo",
+      lastName: "Andersen"
+    };
+  },
+  methods: {
+    changeName: function() {
+      this.firstName = "Mark";
+      this.lastName = "Gonzales";
+    },
+    changeNameSetter: function() {
+      this.fullName = "Mark Gonzales";
+    }
+  },
+
+  computed: {
+    fullName: {
+      get: function() {
+        alert("Assembling full name...");
+        return this.firstName + " " + this.lastName;
+      },
+      set: function(newValue) {
+        alert("Setting new name: " + newValue);
+        var parts = newValue.split(" ");
+        this.firstName = parts[0];
+        this.lastName = parts[parts.length - 1];
+      }
+    }
+  }
+
+  // computed: {
+  //  fullname() {
+  //       return `Full Name : ${this.firstname + " " + this.lastname}`;
+  //     }
+  // }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
